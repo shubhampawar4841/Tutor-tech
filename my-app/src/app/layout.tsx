556b1f429fd9eefb2f8c36a96040df71bac7e4bf
Shubham Pawar <shubham@getraava.com>
2026-01-19 20:22:30 +0530

    added anayltics

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import MobileNav from "@/components/MobileNav";
@@ -38,6 +39,7 @@ export default function RootLayout({
             </main>
           </div>
         </div>
+        <Analytics />
       </body>
     </html>
   );
